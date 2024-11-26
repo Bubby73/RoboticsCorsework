1eb582dbde152ca2e6d5801da5e891f82a26d994
Herbert <09tunafish1285@gmail.com>
2024-11-26 11:00:35 +0000

    .

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ L5 = 0.05
 theta1 = theta4 = theta5 = theta6 = np.radians(0) # Joint Angles initially set to 0
 
 def InitialiseDH(alpha3, alpha6, D2, D3, L0, L1, L2, L3, L4, L5, theta1, theta4, theta5, theta6):
-    # The D-H Table is as follows aplha, a, d, theta:
+    # The D-H Table is as follows d, a, alpha, theta:
     DH = np.array([[L0, 0, 0, 0], # 0
                     [0, 0, 0, theta1], # 1
                     [D2, 0, 0, 0], # 2
@@ -29,10 +29,14 @@ def InitialiseDH(alpha3, alpha6, D2, D3, L0, L1, L2, L3, L4, L5, theta1, theta4,
 
 DH = InitialiseDH(alpha3, alpha6, D2, D3, L0, L1, L2, L3, L4, L5, theta1, theta4, theta5, theta6)
 
-
 robot = rtb.SerialLink(
     [
-        rtb
+        rtb.RevoluteDH(d=DH[1][0], a = DH[1][1], alpha = DH[1][2], offset = DH[1][3]),
+        rtb.PrismaticDH(d = DH[2][0], a = DH[2][1], alpha = DH[2][2], offset = DH[2][3]),
+        rtb.PrismaticDH(d = DH[3][0], a = DH[3][1], alpha = DH[3][2], offset = DH[3][3]),
+        rtb.RevoluteDH(d=DH[4][0], a = DH[4][1], alpha = DH[4][2], offset = DH[4][3]),
+        rtb.RevoluteDH(d=DH[5][0], a = DH[5][1], alpha = DH[5][2], offset = DH[5][3]),
+        rtb.RevoluteDH(d=DH[6][0], a = DH[6][1], alpha = DH[6][2], offset = DH[6][3]),
     ]
 )
  
