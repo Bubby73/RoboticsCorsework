4401a596760ee9b7723a09d00d8193c151fd5d48
Herbert <09tunafish1285@gmail.com>
2024-12-07 18:34:36 +0000

    Q3 done

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Import the necessary libraries
 import numpy as np
+from numpy import random
 import roboticstoolbox as rtb
 from spatialmath import SE3
 import matplotlib.pyplot as plt
-import random
 
 
 # Define all variables used in the code including join variables, target poses and transformation matricies
@@ -12,6 +12,18 @@ import random
 L0, L1, L2, L3, L4, L5 = 0.1, 0.2, 0.3, 0.3, 0.1, 0.05 
 alpha3 = alpha6 = np.pi/2                    
 theta1, d2, d3, theta4, theta5, theta6 = 0, 0.5, 0, 0, 0, 0  # Initial joint variables
+
+# Define DH table (Alpha, d, a, Theta)
+DH = [
+    [0, L0, 0, 0],  # Base (Revolute)
+    [0, 0, 0, 0],   # Joint 1 (Revolute)
+    [0, 0, 0, 0],       # Joint 2 (Prismatic)
+    [alpha3, 0, 0, 0],       # Joint 3 (Prismatic)
+    [0, L1, 0, 0], # Joint 4 (Revolute)
+    [0, L5, L2, 0],  # Joint 5 (Revolute)
+    [alpha6, L3, 0, 0],  # Joint 6 (Revolute)
+    [0, L4, 0, 0]        # End-effector
+]
 
 # Define target poses {Part 2}
 target_1 = [[1, 0, 0, -0.3],
@@ -36,21 +48,28 @@ btT_f = np.array([[0.6791, -0.6403, 0.359, -0.4475],
          [0.359, 0.7162, 0.5985, 0.599],
          [0, 0, 0, 1]])
 
-mass = 0.2
+mass = 0.2 # Define mass and gravity constants
+g = 9.81 
 
 
-# Define DH table (Alpha, d, a, Theta)
-DH = [
-    [0, L0, 0, 0],  # Base (Revolute)
-    [0, 0, 0, theta1],   # Joint 1 (Revolute)
-    [0, d2, 0, 0],       # Joint 2 (Prismatic)
-    [alpha3, d3, 0, 0],       # Joint 3 (Prismatic)
-    [0, L1, 0, theta4], # Joint 4 (Revolute)
-    [0, L5, L2, theta5],  # Joint 5 (Revolute)
-    [alpha6, L3, 0, theta6],  # Joint 6 (Revolute)
-    [0, L4, 0, 0]        # End-effector
-]
+# Define parameters for part 4, centres of mass, link masses, tensor manipulator links, load, {Part 4}
+CoMs = [[0, 0, 0.5], [0, 0, 0.3], [0.0526, -1.526e-4, 0.4998], 
+        [0.0526, -1.526e-4, 0.4998], [0.2203, 0.1271, 0.4761], 
+        [0.2208, 0.2812, 0.2578], [0.2207, 0.2671, 0.0583]]
 
+linkMasses = [6.16, 9.81, 4.767, 4.767, 3.7632, 1.96, 0]
+
+inertial_Manipulator_links = [[[0.0224, 0, 0], [0, 0.0224, 0], [0, 0, 0.0077]],
+                              [[1.088, 0, 0], [0, 1.0882, 0], [0, 0, 0.004]],
+                              [[1.1932, 0.0009, -0.1254], [0.0009, 1.2268, 0.0003], [-0.1254, 0.0003, 0.0357]],
+                              [[1.1932, 0.0009, -0.1254], [0.0009, 1.2268, 0.0003], [-0.1254, 0.0003, 0.0357]],
+                              [[0.9429, -0.1055, -0.3949], [-0.1055, 1.038, -0.2229], [-0.3949,-0.2229, 0.2714]],
+                              [[0.3116, -0.1217, -0.1116], [-0.1217, 0.252, -0.144], [-0.1116, -0.144, 0.2509]],
+                              [[0.0110, -0.0087, -0.0019], [-0.0087, 0.0077, -0.0023], [-0.0019, -0.0023, 0.00177]]]
+
+load = 0.2
+
+#pose_d = [pi.]
 
 # Define the robot taking values from the DH table
 robot = rtb.DHRobot([
@@ -69,51 +88,32 @@ robot.tool = SE3(0, 0, L4) # Set the tool of the robot
 # Define example joint values {Part 1}
 joint_values = [theta1, d2, d3, theta4, theta5, theta6]  
 
-# Compute the forward kinematics
-T = robot.base * robot.fkine(joint_values) * robot.tool
-
 # Function to compute and print link poses
 def computeLinkPoses(robot, joint_values):
-    T_base = robot.base
+    T_Pose = robot.base # start from the robot base
+    for i in range(len(joint_values)):
+        Tn = robot[i].A(joint_values[i]) # Calcuate the DH transformation matrix from the givern joint angle
+        T_Pose = T_Pose * Tn
+        print("\nTransformation matrix from base to joint ",i+1,":\n", T_Pose) # Output link pose relative to the robot base
 
-    T1 = robot[0].A(joint_values[0])
-    T_base_1 = T_base * T1
-    print("\nTransformation matrix from base to joint 1:\n", T_base_1)
 
-    T2 = robot[1].A(joint_values[1])
-    T_base_2 = T_base_1 * T2
-    print("\nTransformation matrix from base to joint 2:\n", T_base_2)
-
-    T3 = robot[2].A(joint_values[2])
-    T_base_3 = T_base_2 * T3
-    print("\nTransformation matrix from base to joint 3:\n", T_base_3)
-
-    T4 = robot[3].A(joint_values[3])
-    T_base_4 = T_base_3 * T4
-    print("\nTransformation matrix from base to joint 4:\n", T_base_4)
-
-    T5 = robot[4].A(joint_values[4])
-    T_base_5 = T_base_4 * T5
-    print("\nTransformation matrix from base to joint 5:\n", T_base_5)
-
-    T6 = robot[5].A(joint_values[5])
-    T_base_6 = T_base_5 * T6
-    print("\nTransformation matrix from base to joint 6:\n", T_base_6)
-
-    T_tool = robot.tool
-    T_base_tool = T_base_6 * T_tool
+    T_tool = robot.tool # end effector is calculated seperatly
+    T_base_tool = T_Pose * T_tool
     print("\nTransformation matrix from base to tool:\n", T_base_tool)
+
+# Compute the forward kinematics
+T = robot.base * robot.fkine(joint_values) * robot.tool
 
 
 # Function to compute and print inverse kinematics solution(s) {Part 2}
 def compute_inverse_kinematics(robot, target_pose, guess_values):
     target_SE3 = SE3(target_pose) # Convert target pose to SE3 format
-    success = False
     for i in range(len(guess_values)):
         q_initial = guess_values[i] # Initial guess for the joint values
         solutions = robot.ikine_LM(target_SE3, q0 = q_initial, joint_limits=True) # Compute inverse kinematics
-
-        roundedSol = np.round(np.clip(solutions.q, robot.qlim[0], robot.qlim[1]), 3) # round to 3 decimal places and clip to joint limits
+        clippedSol = np.clip(solutions.q, robot.qlim[0], robot.qlim[1]) # Clip joints to their limits
+        roundedSol = np.round([np.degrees(clippedSol[0]), clippedSol[1], clippedSol[2], np.degrees(clippedSol[3]),
+                                np.degrees(clippedSol[4]), np.degrees(clippedSol[5])], 3) # round to 3 decimal places and convert radians to degrees
         
         if solutions.success: # Check if a solution is found
             print('\033[32mInverse kinematics solution within join limits not found for guess:\033[0m',i + 1)
@@ -136,28 +136,22 @@ joint_velocities = [np.radians(15), 0, 0.1, np.radians(-30), np.radians(15), np.
 
 
 # Compute the Jacobian in the base frame
-def jacobians(robot, joint_values_v, Trans, mass):
-    J = robot.jacob0(joint_values_v)
+def jacobians(robot, pose, mass, g):
+    J = robot.jacob0(joint_values_v) # Calculate the jacobian matrix relative to the robot base
     print("\nJacobian matrix in the base frame of the robot:\n", np.round(J, 3))
 
     # Compute the end-effector velocities (linear and angular)
-    velocities = J @ joint_velocities
+    velocities = J @ joint_velocities # matricies multiplied to find velocities
     linear_velocity = velocities[:3] # Extract linear velocity
     angular_velocity = velocities[3:] # Extract angular velocity
     print("\nEnd-effector linear velocities  [X, Y, Z]:\n", np.round(linear_velocity, 3))
     print("\nEnd-effector angular velocities [Wx, Wy,Wz]:\n", np.round(angular_velocity, 3))
 
-    F_ee = mass * np.array([0, 0, -9.81]) # Compute the end-effector force
-    R = Trans[:3, :3] # Extract the rotation matrix from the transformation matrix
-    F_base = R @ F_ee # Transform the end-effector force to the base frame
-    print("\nEnd-effector force in the base frame:\n", np.round(F_base, 3))
+    J = robot.jacob0(robot.ikine_LM(pose).q) # find the inverse kinematics of the static pose, then find the jacobian matrix
+    F_ee = mass * np.array([0, 0, -g, 0, 0, 0]) # Compute the end-effector force
+    tau = J.T @ F_ee # Compute the joint torques
+    print("Joint torques/forces due to static load:", np.round(tau, 3))
 
-    q = robot.ikine_LM(Trans).q
-    J = robot.jacob0(q)
-    print(J.T)
-    print(F_base)
-    #tau = J.T @ F_base # Compute the joint torques
-    #print("Joint torques/forces due to static load:", tau)
 
 
 
@@ -173,15 +167,20 @@ print("\nForward kinematics:\n", T)
 print("\nLink poses:")
 computeLinkPoses(robot, joint_values) # Compute link poses
 
+# Plot the robot
+robot.plot(joint_values, block=True)
+plt.show()
+
+
 # {Part 2}
 print("\nInverse kinematics for each target:")
 # Compute inverse kinematics for the target poses
 # Generate 5 random guesses for the joint values
 guess = []
 guesses = []
-for i in range(5):
+for i in range(10):
     for i in range(6):
-        guess.append(random.uniform(-100, 100) / 100)
+        guess.append(random.rand()) # append a random float between 0 and 1
     guesses.append(guess)
     guess = []
 
@@ -194,9 +193,6 @@ compute_inverse_kinematics(robot, target_3, guesses)
 
 
 # Jacobians, Velocity and Static Force Analysis {Part 3} 
-jacobians(robot, joint_values_v, btT_f, mass) 
+jacobians(robot, btT_f, mass, g) 
 
 
-# Plot the robot
-robot.plot(joint_values, block=True)
-plt.show()
